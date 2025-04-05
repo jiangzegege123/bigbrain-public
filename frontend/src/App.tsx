1ce2461072158fc807807241557bf1477009ff97
Alfie728 <zegro728@gmail.com>
2025-04-05 22:37:57 +1100

    added react router for routing and tailwind for styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,15 +7,15 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="">
         <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-red-600 text-lg">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setup.ts',
+    environment: "jsdom",
+    setupFiles: "./src/setup.ts",
   },
   server: {
     port: 3000,
   },
-})
-
+});
